94fd552f7233a41b65d5af6a7624215b93beb2a7
ankitkumarjsr <ankitkumar259stmary@gmail.com>
2022-09-17 17:23:58 +0530

    string interpolation

--- print_statement.dart
+++ print_statement.dart
@@ -1,4 +1,6 @@
 void main() {
   print("Hello Flutter"); // Normal print statement\
   print("Ankit" + " " + "Kumar"); // Concatenation of two strings
+  String name = 'Ankit';
+  print("$name is a good boy"); // String interpolation
 }
