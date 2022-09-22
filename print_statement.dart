15c646c60057926f9248924030de408e5f8db991
ankitkumarjsr <ankitkumar259stmary@gmail.com>
2022-09-22 10:56:04 +0530

    names checking in different way

--- print_statement.dart
+++ print_statement.dart
@@ -1,6 +1,14 @@
 void main() {
   //  working  with lists in dart
-  List<String> names = ['ankit', 'paji','subho','vishal', 'jack', 'jassp', 'salman'];
+  List<String> names = [
+    'ankit',
+    'paji',
+    'subho',
+    'vishal',
+    'jack',
+    'jassp',
+    'salman'
+  ];
 
   // when we only need to know if element is present or not
   // boolean type return hoga
@@ -10,4 +18,14 @@ void main() {
   // IF not found then it returns  -1
   int foundIndex = names.indexOf('pajJi');
   print(foundIndex);
+
+  // if we want to get those people names whose names starts with specific character
+  List<String> namesStartWithJ =
+      names.where((element) => element.startsWith('j')).toList();
+
+// if any randon check karna hai ke names containing these elements
+  List<String> namescontainsI =
+      names.where((element) => element.contains('i')).toList();
+
+  print(namesStartWithJ);
 }
