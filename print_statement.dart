10b6765bbef9a5ce9710eeab4c6ddd302b62252d
ankitkumarjsr <ankitkumar259stmary@gmail.com>
2022-09-22 10:18:01 +0530

    work with lists - add, remove,insert,length

--- print_statement.dart
+++ print_statement.dart
@@ -1,6 +1,29 @@
 void main() {
-  print("Hello Flutter"); // Normal print statement\
-  print("Ankit" + " " + "Kumar"); // Concatenation of two strings
-  String name = 'Ankit';
-  print("$name is a good boy"); // String interpolation
+
+  //  working  with lists in dart
+  List<String> names = [];
+
+  // adding elements in list
+  names.add('first');
+  names.add('second');
+
+  // size
+  print(names.length);
+
+  // print list
+  print('list=$names');
+
+  // remove
+  names.remove('first');
+
+  print('list=$names');
+
+  // if remove at specific index
+  names.removeAt(0);
+
+  print('list=$names');
+
+  // if specific position ma kuch ghusana hai
+  names.insert(0, 'three');
+  print('list=$names');
 }
