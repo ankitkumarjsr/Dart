2bd0fb27bea1f441932490ec99145517609a1403
ankitkumarjsr <ankitkumar259stmary@gmail.com>
2022-09-24 16:51:27 +0530

    Maps in  Dart

---
+++ map.dart
@@ -0,0 +1,19 @@
+void main() {
+  Map<String, int> student = {};
+  // input in map
+  student["Ankit"] = 121;
+  student["subho"] = 122;
+  student["sayan"] = 123;
+  student["vishal"] = 1250;
+  student["paji"] = 1885;
+  // print(student["paji"]);
+
+  // to print whole map at once
+  print(student);
+  
+  //To print the keys of map 
+  print(student.keys);
+
+  // to print the values of map
+  print(student.values);
+}
